05da1638b38f89a032a5811ce770a06af652e30b
steve9948 <mbuguagichimu@gmail.com>
2024-05-10 08:52:54 +0000

    Average weighted score for all!

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,7 +1,6 @@
 -- creates a stored procedure ComputeAverageWeightedScoreForUsers that
 --- computes and store the average weighted score for all students
 
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
